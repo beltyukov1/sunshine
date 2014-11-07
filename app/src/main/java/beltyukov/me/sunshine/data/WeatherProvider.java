4cf85e6851c371a6734d5959823fd8ce95a431e8
Alex Beltyukov <beltyukov.1@gmail.com>
2014-11-07 13:15:13 -0600

    Add bulk insert capability to content provider

--- app/src/main/java/beltyukov/me/sunshine/data/WeatherProvider.java
+++ app/src/main/java/beltyukov/me/sunshine/data/WeatherProvider.java
@@ -260,6 +260,33 @@ public class WeatherProvider extends ContentProvider {
         return rowsUpdated;
     }
 
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        final SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case WEATHER:
+                db.beginTransaction();
+                int returnCount = 0;
+                try {
+                    for (ContentValues value : values) {
+                        long _id = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, value);
+                        if (_id != -1) {
+                            returnCount++;
+                        }
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+                getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+            default:
+                return super.bulkInsert(uri, values);
+        }
+    }
+
     private static UriMatcher buldUriMatcher() {
         final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
         final String authority = WeatherContract.CONTENT_AUTHORITY;
