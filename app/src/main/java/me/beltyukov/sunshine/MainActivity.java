e03837cca2089c094ebce259ef081e25ed92ce53
Alex Beltyukov <beltyukov.1@gmail.com>
2015-05-14 19:35:11 -0500

    Continue implementing redlines

--- app/src/main/java/me/beltyukov/sunshine/MainActivity.java
+++ app/src/main/java/me/beltyukov/sunshine/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
             }
         } else {
             mTwoPane = false;
-            getSupportActionBar().setElevation(0f);
         }
 
         ForecastFragment forecastFragment = (ForecastFragment) getFragmentManager().findFragmentById(R.id.fragment_forecast);
